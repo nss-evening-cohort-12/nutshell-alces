37b49cd30fecb2893ac8cfa6d9186b04711e17cd
Brooke Nemchak <bnemchak@gmail.com>
2020-07-16 19:11:25 -0500

    skeleton file setup

---
+++ src/javascripts/helpers/utils.js
@@ -0,0 +1,19 @@
+const printToDom = (selector, text) => {
+  $(selector).html(text);
+};
+
+const convertFirebaseCollection = (data) => {
+  const objectCollection = data;
+  const arrayCollection = [];
+
+  if (objectCollection) {
+    Object.keys(objectCollection).forEach((itemId) => {
+      objectCollection[itemId].id = itemId;
+      arrayCollection.push(objectCollection[itemId]);
+    });
+  }
+
+  return arrayCollection;
+};
+
+export default { printToDom, convertFirebaseCollection }
