533c798a7e692281b6067a3fc3b2c730ff486f8f
Phonesalo Sihanorak <61893644+psihanorak@users.noreply.github.com>
2020-07-18 08:52:44 -0500

    Update planesList.js

--- src/javascripts/components/planesList/planesList.js
+++ src/javascripts/components/planesList/planesList.js
@@ -1,7 +1,6 @@
 import planesData from '../../helpers/data/planesData';
 import planesComponent from '../planes/planes';
 import utils from '../../helpers/utils';
-@import "../javascripts/components/planes/planes.scss";
 
 const showPlanes = () => {
   planesData.getPlanes()
