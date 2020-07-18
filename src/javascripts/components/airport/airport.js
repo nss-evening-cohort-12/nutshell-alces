5fbec4f7c047cf74ebfa52a59bef2a688e010d5b
Mark Young <markyaaronyoung@gmail.com>
2020-07-18 01:53:59 -0500

    create airport component to display all the airports

---
+++ src/javascripts/components/airport/airport.js
@@ -0,0 +1,18 @@
+import './airport.scss';
+
+const airportMaker = (airport) => {
+  const domString = `
+    <div class="airport-card">
+      <img class="card-img-top" src="${airport.imgURL}" alt="Card image cap">
+      <div class="airport-body">
+        <h5 class="airport-name">Name: ${airport.name}</h5>
+        <p class="airport-location">Location: ${airport.location}</p>
+        <a href="${airport.webURL}">${airport.name}</a>
+      </div>
+    </div>
+  `;
+
+  return domString;
+};
+
+export default { airportMaker };
--- src/javascripts/components/airportList/airportList.js
+++ src/javascripts/components/airportList/airportList.js
@@ -1,9 +1,24 @@
+import airportComponent from '../airport/airport';
 import airportData from '../../helpers/data/airportData';
-// import utils from '../../helpers/utils';
+import utils from '../../helpers/utils';
 
 const buildHangar = () => {
   airportData.getAirports()
-    .then((airports) => console.warn('Get airports worked', airports))
+    .then((airports) => {
+      console.warn('Get airports worked', airports);
+      let domString = `
+        <h2 class="text-center">Hangar</h2>
+        <div class="d-flex flex-wrap">
+      `;
+
+      airports.forEach((airport) => {
+        domString += airportComponent.airportMaker(airport);
+      });
+
+      domString += '</div>';
+
+      utils.printToDom('#hangar', domString);
+    })
     .catch((err) => console.error('get airports broke', err));
   // const domString = '<h1>I SEE AIRPORTS</h1>';
   // utils.printToDom('#hangar', domString);
