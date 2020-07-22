809fd0749e2bc182f246cc930c76d41af53860d8
Jim Browning <jamalebrowning@gmail.com>
2020-07-21 20:08:01 -0500

    wip

--- src/javascripts/components/landingPage/landingPage.js
+++ src/javascripts/components/landingPage/landingPage.js
@@ -8,25 +8,44 @@ const buildLandingPage = () => {
       <img src="https://i.ibb.co/1fHqVpB/PAN-AM-LOGO.png" alt="PAN-AM-LOGO" border="0">    </div>
     <div class="nav-buttons">
       <div class="airport-nav">
-        <button type="button" class="btn btn-primary btn-lg">Airports</button>
+        <button type="button" class="btn btn-primary btn-md lNav">Airports</button>
       </div>
       <div class="plane-nav">
-        <button type="button" class="btn btn-primary btn-lg">Planes</button>
+        <button type="button" class="btn btn-primary btn-md lNav">Planes</button>
       </div>
       <div class="crew-nav">
-        <button type="button" class="btn btn-primary btn-lg">Crew</button>
+        <button type="button" class="btn btn-primary btn-md lNav">Crew</button>
       </div>
       <div class="food-nav">
-        <button id="see-foods" type="button" class="btn btn-primary btn-lg">Food</button>
+        <button id="see-foods" type="button" class="btn btn-primary btn-md lNav">Food</button>
       </div>
     </div>
    `;
-  utils.printToDom('#homepage', domString);
+  utils.printToDom('#landingPage', domString);
 };
-
+const buildLandingPageButtons = () => {
+  const domString = `
+   <div class="main-section">
+    <div class="nav-buttons">
+      <div class="airport-nav">
+        <button type="button" class="btn btn-primary btn-md lNav">Airports</button>
+      </div>
+      <div class="plane-nav">
+        <button type="button" class="btn btn-primary btn-md lNav">Planes</button>
+      </div>
+      <div class="crew-nav">
+        <button type="button" class="btn btn-primary btn-md lNav">Crew</button>
+      </div>
+      <div class="food-nav">
+        <button id="see-foods" type="button" class="btn btn-primary btn-md lNav">Food</button>
+      </div>
+    </div>
+   `;
+  utils.printToDom('#landingPage', domString);
+};
 const hideLanding = () => {
   const domString = '<div class="empty-landing"></div>';
-  utils.printToDom('#homepage', domString);
+  utils.printToDom('#landingPage', domString);
 };
 
 const resetPage = () => {
@@ -36,4 +55,9 @@ const resetPage = () => {
     $('#test-hide').addClass('hide');
   });
 };
-export default { buildLandingPage, hideLanding, resetPage };
+export default {
+  buildLandingPage,
+  hideLanding,
+  resetPage,
+  buildLandingPageButtons,
+};
