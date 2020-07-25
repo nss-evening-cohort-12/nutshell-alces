45add0ea706953973954544b0130d2eedb9dfee0
Jim Browning <jamalebrowning@gmail.com>
2020-07-25 12:30:40 -0500

    complete auth for dashboard

--- src/javascripts/components/myNavbar/myNavbar.js
+++ src/javascripts/components/myNavbar/myNavbar.js
@@ -1,9 +1,11 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import landing from '../landingPage/landingPage';
 
 const logoutEvent = () => {
   $('#navbar-logout-button').click((e) => {
     e.preventDefault();
+    landing.viewNoDashboard();
     firebase.auth().signOut();
   });
 };
