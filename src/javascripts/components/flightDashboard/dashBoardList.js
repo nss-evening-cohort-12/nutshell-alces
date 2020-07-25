248f7c86d096f9f6f47047c5e70a021c226cfd04
Billy Campbell <wcampbell91@gmail.com>
2020-07-25 13:52:42 -0500

    added destination div and some styling

--- src/javascripts/components/flightDashboard/dashBoardList.js
+++ src/javascripts/components/flightDashboard/dashBoardList.js
@@ -1,11 +1,13 @@
 // import utils from '../../helpers/utils';
 import hideLanding from '../landingPage/landingPage';
 import pilotDiv from './pilots';
+import flightPath from './flightPath/flightPath';
 
 const showDashboard = (e) => {
   e.preventDefault();
   hideLanding.hideLanding();
   pilotDiv.buildPilotDiv();
+  flightPath.buildDestinationDiv();
 };
 
 const dashEvents = () => {
