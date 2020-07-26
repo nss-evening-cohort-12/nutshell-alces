0eaeaee9e40c9d9e1aba6e8e8bd668cd9a5736f3
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-07-26 17:44:12 -0500

    wip

--- src/javascripts/components/flightDashboard/addFlights/addFlightLanding.js
+++ src/javascripts/components/flightDashboard/addFlights/addFlightLanding.js
@@ -1,12 +1,14 @@
 import utils from '../../../helpers/utils';
 import flightPath from '../flightPath/flightPath';
 import pilots from '../crew/pilots';
+import planes from '../planes/flightPlanes';
 
 // THIS FUNCTION WILL HIDE ALL DIVS RELATED TO ADD FLIGHTS
 const hideAddFlights = () => {
   utils.printToDom('#add-flights', '');
   utils.printToDom('#destination', '');
   utils.printToDom('#crew', '');
+  utils.printToDom('#planes', '');
 };
 
 const buildAddFlightLanding = () => {
@@ -18,6 +20,7 @@ const buildAddFlightLanding = () => {
   // ADD NEW DIVS TO ADDFLIGHTS HERE
   pilots.buildPilotDiv();
   flightPath.buildDestinationDiv();
+  planes.buildPlanesDiv();
 };
 
 export default { buildAddFlightLanding, hideAddFlights };
---
+++ src/javascripts/components/flightDashboard/planes/flightPlanes.js
@@ -0,0 +1,34 @@
+import getPlanes from '../../../helpers/data/planesData';
+
+import utils from '../../../helpers/utils';
+
+import './flightPlanes.scss';
+
+const buildPlanesDiv = () => {
+  getPlanes.getPlanes()
+    .then((planes) => {
+      let domString = `
+  <div class="flightPlanes">
+    <h2 class="text-center header">Airplane</h2>
+    <div class="text-center dropdown">
+      <button class="btn btn-secondary dropdown-toggle" type="button" id="planeDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Select
+      </button>
+      <div class="dropdown-menu" aria-labelledby="planeDropdown">`;
+
+      planes.forEach((plane) => {
+        domString += `
+        <a class="dropdown-item" href="#">${plane.type}</a>
+      `;
+      });
+      domString += `
+      </div>
+    </div>
+  </div>
+  `;
+      utils.printToDom('#planes', domString);
+    })
+    .catch((err) => console.error(err));
+};
+
+export default { buildPlanesDiv };
