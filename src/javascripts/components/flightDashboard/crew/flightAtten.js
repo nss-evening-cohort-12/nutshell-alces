f656f2b6cfb9c0ed0d7a9a8ad03c9af9ec23aff6
Jim Browning <jamalebrowning@gmail.com>
2020-07-27 12:19:55 -0500

    fixed crew info

--- src/javascripts/components/flightDashboard/crew/flightAtten.js
+++ src/javascripts/components/flightDashboard/crew/flightAtten.js
@@ -1,56 +1,75 @@
 import utils from '../../../helpers/utils';
 import './crew.scss';
+import pilots from '../../../helpers/data/crewData';
 
 const buildFaDiv = () => {
-  const domString = `
-  
-  <div class="crewFas">
-  <h2 class="text-center">Flight Attendants</h2>
-    <div class="d-flex justify-content-center">
-      <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Attendant
-        </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </div>
-      <div class="dropdown">
-      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Attendant
-      </button>
-      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <a class="dropdown-item" href="#">Action</a>
-        <a class="dropdown-item" href="#">Another action</a>
-        <a class="dropdown-item" href="#">Something else here</a>
-      </div>
-      </div>
-      <div class="dropdown">
-      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Attendant
-      </button>
-      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <a class="dropdown-item" href="#">Action</a>
-        <a class="dropdown-item" href="#">Another action</a>
-        <a class="dropdown-item" href="#">Something else here</a>
-      </div>
-      </div>
-      <div class="dropdown">
-      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Attendant
-      </button>
-      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <a class="dropdown-item" href="#">Action</a>
-        <a class="dropdown-item" href="#">Another action</a>
-        <a class="dropdown-item" href="#">Something else here</a>
-      </div>
-      </div>
-  </div>
-  
-  `;
-  utils.printToDom('#crewF', domString);
+  pilots.getCrew()
+    .then((crew) => {
+      let domString = `
+      <div class="crewFas">
+      <h2 class="text-center">Pilots</h2>
+       <div class="d-flex justify-content-center flex-column">
+       <div class="input-group mb-3">
+       <div class="input-group-prepend">
+         <label class="input-group-text" for="inputGroupSelect01">Attendant</label>
+       </div>
+       <select class="custom-select" id="inputGroupSelect01">
+         <option selected>Choose...</option>`;
+      crew.forEach((member) => {
+        if (`${member.title}` === 'Air Stewardess') {
+          domString += `<option value="${member.id}">${member.name}: ${member.title}</option>`;
+        }
+      });
+      domString += `  </select>
+      
+      </div>`;
+      domString += `
+      <div class="input-group mb-3">
+       <div class="input-group-prepend">
+         <label class="input-group-text" for="inputGroupSelect01">Attendant</label>
+       </div>
+       <select class="custom-select" id="inputGroupSelect01">
+         <option selected>Choose...</option>`;
+      crew.forEach((member) => {
+        if (`${member.title}` === 'Air Stewardess') {
+          domString += `<option value="${member.id}">${member.name}: ${member.title}</option>`;
+        }
+      });
+      domString += `  </select>
+      
+      </div>`;
+      domString += `
+      <div class="input-group mb-3">
+       <div class="input-group-prepend">
+         <label class="input-group-text" for="inputGroupSelect01">Attendant</label>
+       </div>
+       <select class="custom-select" id="inputGroupSelect01">
+         <option selected>Choose...</option>`;
+      crew.forEach((member) => {
+        if (`${member.title}` === 'Air Stewardess') {
+          domString += `<option value="${member.id}">${member.name}: ${member.title}</option>`;
+        }
+      });
+      domString += `  </select>
+      </div>`;
+      domString += `
+      <div class="input-group mb-3">
+       <div class="input-group-prepend">
+         <label class="input-group-text" for="inputGroupSelect01">Attendant</label>
+       </div>
+       <select class="custom-select" id="inputGroupSelect01">
+         <option selected>Choose...</option>`;
+      crew.forEach((member) => {
+        if (`${member.title}` === 'Air Stewardess') {
+          domString += `<option value="${member.id}">${member.name}: ${member.title}</option>`;
+        }
+      });
+      domString += `  </select>
+          </div>
+         </div>
+       </div>`;
+      utils.printToDom('#crewF', domString);
+    });
 };
 
 export default {
